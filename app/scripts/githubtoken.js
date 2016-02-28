e2eb0d1ae62ff7e7a54f82e1276cf16890e25457
Bruemor <arymer1@gmail.com>
2016-02-28 15:22:51 -0500

    pulled in the profile data through ajax and added the footer

--- app/scripts/githubtoken.js
+++ app/scripts/githubtoken.js
@@ -1,1 +1,1 @@
-module.exports = {'token': 'a0296c36a7a83e977bef7c270e073125129b81ec'};
+module.exports = {'token': '976edd8f95d0fd307c8b2a5562a0189d87d9d77e'};
--- app/scripts/index.js
+++ app/scripts/index.js
@@ -4,6 +4,7 @@ var $ = require('jquery');
 var _ = require('underscore');
 var handlebars = require('handlebars');
 var githubtoken = require('./githubtoken.js').token;
+var moment = require('moment');
 var usersUrl = 'https://api.github.com/users/Bruemor33';
 var userReopUrl = 'https://api.github.com/users/Bruemor33/repos';
 
@@ -20,22 +21,43 @@ $('.nav-tabs').click(function(){
 });
 
 
-function getRepos(){
-  $.ajax(userReopUrl).done(function(data){
-    console.log(data);
-    $.each(data, function(index, repo){
-      var repoContext = {
+function getRepos(){ //create the function to make a get request to the repo url of github's api
+  $.ajax(userReopUrl).done(function(data){ //the actual get request
+    console.log(data); //log the data to the console in order to find what I need
+    $.each(data, function(index, repo){ //run a loop through the data needed in order to push every repo to the page
+      var repoContext = { //create a visual list of each data item you want to place within your handlebars template
         repoName: repo.name,
         updatedAt: repo.updated_at,
         lang: repo.language,
         starGaze: repo.stargazers_count,
-        forking: repo.forks_count
+        forking: repo.forks_count,
+        repoUrl: repo.html_url
       };
-      var source = $('#repo-page').html();
-      var template = handlebars.compile(source);
-      var html = template(repoContext);
-      $('#repositories').append(html);
+      var source = $('#repo-page').html(); //where our handlebars script begins, or resides in our html
+      var template = handlebars.compile(source); //the compile magic of our template
+      var html = template(repoContext); //pulling out data into the template
+      //moment(repoContext.updatedAt).startOf('minute').fromNow().append(html); //not really sure on where to place this.
+      $('#repositories').append(html); //appending our data to the placeholder div in our html layout.
+
     });
   });
 };
-getRepos();
+getRepos(); //function called
+
+function getProfile(){ //since I am just pulling profile information, there is no need to run a loop function.
+  $.ajax(usersUrl).done(function(data){
+    console.log(data)
+      var profileContent = { //profile content needed to populate the template
+        profilePic: data.avatar_url,
+        fullName: data.name,
+        userName: data.login,
+        userEmail: data.email,
+        joinedAt: data.created_at
+      };
+      var source = $('#profile-page').html();
+      var template = handlebars.compile(source);
+      var html = template(profileContent);
+      $('#profile-sidebar').append(html);
+  });
+};
+getProfile(); //function called
